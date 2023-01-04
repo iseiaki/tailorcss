613eaf2b32dc5e1e9d4260f04daad590572d600c
iseiaki <mirokurtanidze6@gmail.com>
2023-01-04 18:14:47 +0400

    changes to files, content

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strconv"
+
+	"github.com/atotto/clipboard"
 )
 
 type Config struct {
+	// importing Json Data
 	Name      string `json:"Name"`
+	Title     string `json:"title"`
 	Authors   string `json:"Authors"`
 	Version   string `json:"Version"`
-	Html   string `json:"htmlfile"`
+	Html      string `json:"htmlfile"`
+
 	MainCSS   struct {
 		Primary   string `json:"Primary"`
 		Icons     string `json:"icons"`
@@ -19,12 +27,27 @@ type Config struct {
 		Navbar    string `json:"navbar"`
 		Animations string `json:"animations"`
 	} `json:"maincss"`
+
 	Display struct {
 		Grid   string `json:"Grid"`
 		Flex   string `json:"Flex"`
 		Block  string `json:"Block"`
 		InlineB string `json:"InlineB"`
 	} `json:"Display"`
+
+	Prebuilt struct {
+        Navbar struct {
+         Links string `json:"links"`
+		 Navclass string `json:"navbarclass"`
+		 ElementClass string `json:"elementclass"`
+		 Content string `json:"content"`
+		 Display string `json:"display"`
+         Justifycontent string `json:"justify-content"`
+         Position string `json:"position"`
+		 Backgroundcolor string `json:"background-color"`
+		}`json:"navbar"`
+	} `json:"Prebuilt-elements"`
+
 	JustifyContent struct {
 		Center       string `json:"center"`
 		SpaceAround  string `json:"space-around"`
@@ -50,13 +73,79 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+    
+	// creating a variable so user could copy after a command
+	navHTML := fmt.Sprintf(`
+		<nav>
+			<ul>
+				<li><a href="#">%s</a></li>
+				<li><a href="#">%s</a></li>
+				<li><a href="#">%s</a></li>
+			</ul>
+		</nav>
+	`, config.Prebuilt.Navbar.Content, config.Prebuilt.Navbar.Content, config.Prebuilt.Navbar.Content)
 
 
-	fmt.Println(config.Name)
-	fmt.Println(config.Html)
-	fmt.Println(config.Authors)
-	fmt.Println(config.Version)
-	fmt.Println(config.MainCSS.Primary)
-	fmt.Println(config.MainCSS.Icons)
-	fmt.Println(config.MainCSS.Style)
+	fmt.Print("Enter Command: ") // taking command
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	command := scanner.Text()
+
+	if command == "navbar -c" {
+		err := clipboard.WriteAll(navHTML)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Copied to clipboard!")
+	}
+
+	if command == "navbar -w" {
+		var output string
+		n , err  := strconv.Atoi(config.Prebuilt.Navbar.Links)
+		fmt.Println(n)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for i := 0; i < n; i++ {
+			output += fmt.Sprintf("\n<li class='%v'><a href=\"#\">%v</a></li>", config.Prebuilt.Navbar.ElementClass, config.Prebuilt.Navbar.Content)
+		}
+		html := fmt.Sprintf(`
+<nav>
+<ul>
+		%s
+</ul>
+</nav>
+	`, output)
+
+	errd := clipboard.WriteAll(html)
+	if errd != nil {
+		fmt.Println(errd)
+		return
+	}
+	fmt.Println("Copied to clipboard!")
+	}
+
+	// Open the file for appending
+	if command == "setup -html" {
+    htmlsetup := fmt.Sprintf(`	
+	<html>
+    <head>
+        <link rel="stylesheet" href="./tailor.css">
+        <title>%v</title>
+    </head>
+    <body>
+
+    </body>
+    </html>`, config.Title)
+
+	// Write the navbar HTML to the file
+	err = ioutil.WriteFile(config.Html, []byte(htmlsetup), 0644,)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("setup is complete! Please check %s\n", config.Html)
+}
 }
